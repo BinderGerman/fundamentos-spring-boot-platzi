e6d2e3dc485534832e09b63db988f87746ebb0a1
Germán Binder <bindergerman@gmail.com>
2022-11-12 13:21:24 -0300

    first commit

---
+++ src/main/java/com/funsamentosplatzi/FunsamentosplatziApplication.java
@@ -0,0 +1,13 @@
+package com.funsamentosplatzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunsamentosplatziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunsamentosplatziApplication.class, args);
+	}
+
+}
